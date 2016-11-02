593cb5cfe5ecb1c2003028bbb41daa00bc64e96d
LiaoQF <liaoqf@ct108.com>
2016-11-02 21:13:27 +0800

    1、加入游戏资源

---
+++ res/Hall/webpages/help/common.js
@@ -0,0 +1,8 @@
+﻿
+window.onselectstart = function () { return false; }
+window.oncopy = function () { return false; }
+
+// setDefaultZoom=function()
+// {
+//     document.body.style.zoom = 0.67;
+// }()
