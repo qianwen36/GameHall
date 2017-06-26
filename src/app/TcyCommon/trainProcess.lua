4b7c87c448b3747f83403d1388a2ccf3d9cbc6a9
liaoqf <liaoqf@ct108.com>
2017-06-26 18:06:00 +0800

    1、future 模式

--- src/app/TcyCommon/trainProcess.lua
+++ src/app/TcyCommon/trainProcess.lua
@@ -3,7 +3,39 @@ local function param_pack( params, callback )
 	return params
 end
 
+local function future( ... )
+	local order = {result = true}
+	local function callback( ... )
+		order.res = { ... }
+		if order.co then
+			coroutine.resume(order.co)
+		end
+	end
+	function order:result()
+		if (self.res == nil) then
+			local co, main = coroutine.running()
+			if not main then self.co = co end
+			coroutine.yield()
+		end
+		return unpack(self.res)
+	end
+	local params = {...}
+	local host, service = params[1], table.remove(params, 2)
+	if type(params[#params]) == 'function' then
+		params = table.remove(params)(params, callback)
+	else
+		params = param_pack(params, callback)
+	end
+	if type(host[service]) == 'function' then
+		host[service](unpack(params))
+		return order
+	else
+		print('service:'..service..' not implement at '..tostring(host))
+	end
+end
 local function asyncall( ... )
+	return future(...):result()
+--[[
 	local co, main = coroutine.running()
 	if main then
 		print('Please use .call(...) in .run(func) context')
@@ -24,6 +56,52 @@ local function asyncall( ... )
 	else
 		print('service:'..service..' not implement at '..tostring(host))
 	end
+--]]
+end
+
+local function http(...)
+	-- local order = http([method,] url [, params])
+	-- status, resp = order:result()
+	local method, url, params
+	if select('#', ...) < 3 then
+		method, url, params = 'GET', ...
+	else
+		method, url, params = ...
+	end
+	local request = cc.XMLHttpRequest:new()
+	request:registerScriptHandler(function()
+		local callback = request.callback
+		if	callback then
+			callback(request.status, request.responseText)
+		end
+	end)
+	function request:script(params, callback)
+		self.callback = callback
+		self.responseType = cc.XMLHTTPREQUEST_RESPONSE_JSON
+		local handler = {}
+		function handler.GET()
+			self:open(method, url..'?'..params, true)
+		end
+		function handler.POST()
+			self:open(method, url, true)
+			self:setRequestHeader("Content-Type","application/x-www-form-urlencoded;")
+			self:send(params)
+		end
+		if (handler[method]) then
+			handler[method]()
+		end
+	end
+	if params and type(params) == 'table' then
+		local function url_encode(params)
+			local pp = {}
+			for k, v in pairs(params) do
+				pp[#pp+1] = k..'='..string.urlencode(tostring(v))
+			end
+			return table.concat(pp, '&')
+		end
+		return future(request, 'script', url_encode(params))
+	end
+	return future(request, 'script')
 end
 
 local function runProcess( ... )
@@ -40,6 +118,8 @@ end
 
 local target = {
 	call = asyncall,
+	book = future,
+	http = http,
 	run = runProcess
 }
 
@@ -49,13 +129,21 @@ return target
 -- example
 local Plugin = plugin.AgentManager:getUserPlugin()
 target.run(function ( ... )
-	local code, msg, info = target.call(Plugin, 'queryThirdInfo', 'weixin')
-	if code == AsyncQueryStatus.kSuccess then
-		dump(info)
-	else
-		print(msg)
+	local code, msg, info = target.call(Plugin, 'queryThirdAccountBindState', 'weixin')
+	if code == ThirdAccountStatus.kBinded then
+		code, msg, info = target.call(Plugin, 'queryThirdInfo', 'weixin')
+		if code == AsyncQueryStatus.kSuccess then
+			dump(info)
+		else
+			print(msg)
+		end
 	end
-	code, msg = target.call(Plugin, 'queryThirdAccountBindState', 'weixin')
+	-- following using future model
+	local order = target.http('http://huodong.uc108.org:922/CheckIn/AddCheckIn', {ActId=‘123’， UserId=77681})---
+	local checkin = order
+	order = target.http('http://huodong.uc108.org:922/floorreward/GetFloorRewardConfig', {actid='666'})
+	local code, resp = order:result()
+	code, resp = checkin:result()
 end)
 
 --]]
